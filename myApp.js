dc77f8ecc20b11445840ee21dbaae183266dcf6e
Hudah Nur <hudahn424@gmail.com>
2023-11-03 16:57:01 -0500

    json assignment

--- myApp.js
+++ myApp.js
@@ -1,5 +1,17 @@
 let express = require('express');
 let app = express();
+console.log("Hello World");
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/json", function(req, res) {
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+      res.json({"message": "HELLO JSON"});
+    } else {
+      res.json({"message": "Hello Json"});
+    }
+});
 
 
 
@@ -7,6 +19,11 @@ let app = express();
 
 
 
+
+
+
+
+module.exports = app;
 
 
 
